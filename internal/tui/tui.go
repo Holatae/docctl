5a230678a3fd9929433fc6c9165e3626c55f309c
Victor Andersson <victor@ured.se>
2026-05-02 22:48:24 +0200

    Most basic test

--- internal/tui/tui.go
+++ internal/tui/tui.go
@@ -4,6 +4,9 @@ import (
 	"docctl/internal/app"
 	"fmt"
 	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/huh"
 )
 
 type AppContext struct {
@@ -11,38 +14,134 @@ type AppContext struct {
 	Cfg *app.Config
 }
 
-func StartTUI() {
+type appState int
+
+const (
+	stateMainMenu appState = iota
+	stateSettingsMenu
+)
+
+type mainModel struct {
+	state appState
+	cwd   string
+	cfg   *app.Config
+
+	// Formulär
+	mainMenu     *huh.Form
+	settingsMenu *huh.Form
+}
+
+func (m mainModel) Init() tea.Cmd {
+	return nil
+}
+
+func (m mainModel) Update(keyMsg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmds []tea.Cmd
+
+	// Check for ctrl + c
+	if keyMsg, ok := keyMsg.(tea.KeyMsg); ok {
+		if keyMsg.Type == tea.KeyCtrlC {
+			return m, tea.Quit
+		}
+	}
+
+	switch m.state {
+
+	case stateMainMenu:
+		form, cmd := m.mainMenu.Update(keyMsg)
+		if f, ok := form.(*huh.Form); ok {
+			m.mainMenu = f
+		}
+		cmds = append(cmds, cmd)
+
+		// Blev menyn klar? (tryckte användaren enter?)
+		if m.mainMenu.State == huh.StateCompleted {
+			action := m.mainMenu.GetString("action")
+
+			if action == "settings" {
+				m.state = stateSettingsMenu
+			}
+		}
+	default:
+		panic("unhandled default case")
+	}
+	return m, tea.Batch(cmds...)
+}
+
+func (m mainModel) View() string {
+	switch m.state {
+	case stateMainMenu:
+		return m.mainMenu.View()
+	default:
+		return "Eeeh något gick fel"
+	}
+}
+
+func createMainMenuForm() *huh.Form {
+	return huh.NewForm(
+		huh.NewGroup(
+			huh.NewSelect[string]().
+				Key("action"). // Sätter en nyckel vi kan hämta med GetString()
+				Title("Huvudmeny").
+				Options(
+					huh.NewOption("⚙️ Inställningar", "settings"),
+					huh.NewOption("❌ Avsluta", "exit"),
+				),
+		),
+	)
+}
+
+func StartTUI() error {
 
 	cwd, _ := os.Getwd()
 	cfg, _ := app.LoadConfig(cwd)
 
-	context := &AppContext{
-		Cwd: cwd,
-		Cfg: &cfg,
+	initialModel := mainModel{
+		state:    stateMainMenu,
+		cwd:      cwd,
+		cfg:      &cfg,
+		mainMenu: createMainMenuForm(),
 	}
 
-	_ = checkFirstRun()
+	p := tea.NewProgram(initialModel, tea.WithAltScreen())
 
-	for {
-
-		action, err := showMainMenu()
-
-		if err != nil || action == "exit" {
-			fmt.Println("Avslutar DocCtl. 👋")
-			return
-		}
-
-		switch action {
-		case "init":
-			_ = runInitFlow()
-		case "build":
-			runActionFlow("build")
-		case "seal":
-			runActionFlow("seal")
-		case "settings":
-			runSettingsFlow(context)
-		}
+	if _, err := p.Run(); err != nil {
+		return err
 	}
+	return nil
+
+	/*
+		context := &AppContext{
+			Cwd: cwd,
+			Cfg: &cfg,
+		}
+
+		_ = checkFirstRun()
+
+		for {
+
+			action, err := showMainMenu()
+
+			if err != nil || action == "exit" {
+				fmt.Println("Avslutar DocCtl. 👋")
+				return
+			}
+
+			switch action {
+			case "init":
+				_ = runInitFlow()
+			case "build":
+				runActionFlow("build")
+			case "seal":
+				runActionFlow("seal")
+			case "settings":
+				runSettingsFlow(context)
+			}
+
+
+		}
+
+	*/
 }
 
 func runSettingsFlow(context *AppContext) {
